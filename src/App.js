7b32e6ae160812168e15f81dfdfafc7bad06980f
G.Harshavardhana Krishna Chaitanya <68343068+harshavardhanakrishnachaitanya2001@users.noreply.github.com>
2023-04-05 21:04:16 +0530

    Started making the youtube layout using grid layout with react and tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Video from './components/Video'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='font-bold text-center p-5 text-4xl'>Youtube layout</h1>
+      <div className='grid grid-cols-6 text-center gap-4'>
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
