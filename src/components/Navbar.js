bab005953a9a94b545d89cca287e97f515bb80f7
harshavardhanakrishnachaitanya2001 <pvageta02@gmail.com>
2023-04-07 00:27:10 +0530

    navbar in a fixed position

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,15 @@ const Navbar = () => {
   return (
     <div className='flex justify-center'>
     <nav className='fixed cursor-pointer bg-'>
-       <ul className='flex gap-2 justify-center'>
-            <li className='bg-gray-100 p-2'>All</li>
-            <li className='bg-gray-100 p-2'>Gaming</li>
-            <li className='bg-gray-100 p-2'>Mixes</li>
-            <li className='bg-gray-100 p-2'>Lions</li>
-            <li className='bg-gray-100 p-2'>Wildlife</li>
-            <li className='bg-gray-100 p-2'>Tamil Cinema</li>
-            <li className='bg-gray-100 p-2'>Music</li>
-            <li className='bg-gray-100 p-2'>Live</li>
+       <ul className='flex gap-2 p-2 justify-center bg-white'>
+            <li className='p-2 bg-gray-200 rounded-lg transition'>All</li>
+            <li className='p-2 bg-gray-200 rounded-lg transition'>Gaming</li>
+            <li className='p-2 bg-gray-200 rounded-lg transition'>Mixes</li>
+            <li className='p-2 bg-gray-200 rounded-lg transition'>Lions</li>
+            <li className='p-2 bg-gray-200 rounded-lg transition'>Wildlife</li>
+            <li className='p-2 bg-gray-200 rounded-lg transition'>Tamil Cinema</li>
+            <li className='p-2 bg-gray-200 rounded-lg transition'>Music</li>
+            <li className='p-2 bg-gray-200 rounded-lg transition'>Live</li>
         </ul> 
     </nav>
     </div>
